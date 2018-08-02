3bfe76bbcce6b3483bd6693f1b7586619af3b17c
Roberto Tobias <sb4fun88@gmail.com>
2018-08-02 14:25:15 -0300

    Update connection.js

--- app/config/connection.js
+++ app/config/connection.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const pool = mysql.createPool({
-  host     : 'mysql.dalchau.com.br',
-  user     : 'dalchau40',
-  password : 'dalchau88',
-  database : 'dalchau40'
+  host     : '',
+  user     : '',
+  password : '',
+  database : ''
 });
 
 module.exports = pool;
